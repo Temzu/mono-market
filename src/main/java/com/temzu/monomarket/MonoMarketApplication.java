7e93d51919d6c720b38833b0f919efc41a11764e
Temzu <40913z@mail.ru>
2022-04-13 08:35:03 +0300

    Initial commit

---
+++ src/main/java/com/temzu/monomarket/MonoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.temzu.monomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoMarketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MonoMarketApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/temzu/monomarket/MonoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temzu.monomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoMarketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
