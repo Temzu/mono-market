dca71dedeef3d37105e387a7799875dc00192013
Temzu <40913z@mail.ru>
2022-04-15 20:15:05 +0300

    added pagination

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -2,7 +2,19 @@ angular.module('market-front', []).controller("indexController",
     function ($scope, $http) {
       const contextPath = "http://localhost:8189/market/api/v1/";
 
-      $http.get(contextPath + 'products').then(function (response) {
-        $scope.products = response.data;
-      });
+      $scope.loadProducts = function (pageIndex = 1) {
+        $http({
+          url: contextPath + 'products',
+          method: 'GET',
+          params: {
+            page: pageIndex
+          }
+        }).then(function (response) {
+          $scope.productsPage = response.data;
+          console.log(response)
+        });
+      }
+
+      $scope.loadProducts(1);
+
     })
