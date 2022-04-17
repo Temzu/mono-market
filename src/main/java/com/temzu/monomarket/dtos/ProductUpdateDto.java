2fbe046c9f44f0ab0942d9ddc9c9ae2f9eeaf75a
Temzu <40913z@mail.ru>
2022-04-17 09:21:33 +0300

    added validation and fixed update method

--- src/main/java/com/temzu/monomarket/dtos/ProductUpdateDto.java
+++ src/main/java/com/temzu/monomarket/dtos/ProductUpdateDto.java
@@ -1,7 +1,6 @@
 package com.temzu.monomarket.dtos;
 
 import java.math.BigDecimal;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
