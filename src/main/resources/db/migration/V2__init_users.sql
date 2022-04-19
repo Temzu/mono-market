b6d5cb5450a02d8005725008ea98f76c980ad529
Temzu <40913z@mail.ru>
2022-04-19 21:08:27 +0300

    added front authentication and pagination

--- src/main/resources/db/migration/V2__init_users.sql
+++ src/main/resources/db/migration/V2__init_users.sql
@@ -27,10 +27,10 @@ insert into roles (name)
 values ('ROLE_USER'),
        ('ROLE_ADMIN');
 
--- password: 123
+-- password: 123456
 insert into users (login, password, email)
-values ('user', '$2a$10$3ivPJN8MyXC/bb0y87FxzuYkzMByRuj555b4DfS3x7EtWM4tzX5u.', 'bob_johnson@gmail.com'),
-       ('admin', '$2a$10$3ivPJN8MyXC/bb0y87FxzuYkzMByRuj555b4DfS3x7EtWM4tzX5u.', 'john_johnson@gmail.com');
+values ('user', '$2a$12$L/aHEr7LfXA6hFQzUI5o/O7ph1jj1gZXXmUwsu3LVkm7KM77WStne', 'bob_johnson@gmail.com'),
+       ('admin', '$2a$12$L/aHEr7LfXA6hFQzUI5o/O7ph1jj1gZXXmUwsu3LVkm7KM77WStne', 'john_johnson@gmail.com');
 
 insert into users_roles (user_id, role_id)
 values (1, 1),
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -21,15 +21,80 @@
     });
   }
 
-  const contextPath = "http://localhost:8189/market/api/v1/";
+  const contextPath = "http://localhost:8189/market";
 
   function run($rootScope, $http, $localStorage) {
+    if ($localStorage.currentUser) {
+      $http.defaults.headers.common.Authorization = 'Bearer '
+          + $localStorage.currentUser.token;
+    }
 
+    $http.post(contextPath + '/api/v1/cart')
+    .then(function successCallback(response) {
+      $localStorage.happyCartUuid = response.data;
+    });
   }
 })();
 
 angular.module('market-front').controller('indexController',
     function ($scope, $http, $localStorage, $location) {
-      const contextPath = "http://localhost:8189/market/api/v1/";
 
+      const contextPath = "http://localhost:8189/market";
+
+      $scope.tryToAuth = function () {
+
+        $http.post(contextPath + '/api/v1/auth/login', $scope.user)
+        .then(function successCallback(response) {
+          if (response.data.token) {
+            $http.defaults.headers.common.Authorization = 'Bearer '
+                + response.data.token;
+            $localStorage.currentUser = {
+              login: $scope.user.login,
+              token: response.data.token
+            };
+
+            $scope.currentUserName = $scope.user.login;
+
+            $scope.user.login = null;
+            $scope.user.password = null;
+          }
+        }, function errorCallback(response) {
+          alert(response.data.messages);
+        });
+
+        // $http.post(contextPath + '/api/v1/cart')
+        // .then(function successCallback(response) {
+        //   $localStorage.happyCartUuid = response.data;
+        // });
+      };
+
+      $scope.tryToLogout = function () {
+        $scope.clearUser();
+
+        // $http.post(contextPath + '/api/v1/cart')
+        // .then(function successCallback(response) {
+        //   $localStorage.happyCartUuid = response.data;
+        // });
+
+        $location.path('/');
+        if ($scope.user.login) {
+          $scope.user.login = null;
+        }
+        if ($scope.user.password) {
+          $scope.user.password = null;
+        }
+      };
+
+      $scope.clearUser = function () {
+        delete $localStorage.currentUser;
+        $http.defaults.headers.common.Authorization = '';
+      };
+
+      $scope.isUserLoggedIn = function () {
+        if ($localStorage.currentUser) {
+          return true;
+        } else {
+          return false;
+        }
+      };
     });
