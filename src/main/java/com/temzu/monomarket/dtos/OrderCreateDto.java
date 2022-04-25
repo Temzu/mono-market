d6b9a0f344cb67aedd9bb4671948360aea645d0e
Temzu <40913z@mail.ru>
2022-04-25 08:54:15 +0300

    update

--- src/main/java/com/temzu/monomarket/dtos/OrderCreateDto.java
+++ src/main/java/com/temzu/monomarket/dtos/OrderCreateDto.java
@@ -2,10 +2,15 @@ package com.temzu.monomarket.dtos;
 
 import java.util.UUID;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class OrderCreateDto {
+
   private UUID cartUuid;
+
   private String address;
+
   private String phone;
 }
